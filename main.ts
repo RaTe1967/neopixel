7e5e4bb8bd61870de8cc97c7a9b51080910e69bd
RaTe1967 <115639425+RaTe1967@users.noreply.github.com>
2022-10-14 12:43:29 +0200

    Aktualizovat pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     jedna_dioda_kruh()
 })
-function tocici_duha () {
+function tocici_duha() {
     diody.showRainbow(1, 360)
     for (let index = 0; index < 10; index++) {
         diody.show()
@@ -9,8 +9,9 @@ function tocici_duha () {
         basic.pause(100)
     }
 }
-function jedna_dioda_kruh () {
-    for (let index2 = 0; index2 <= 23; index2++) {
+
+function jedna_dioda_kruh() {
+    for (let index2 = 0; index2 < 24; index2++) {
         diody.setPixelColor(index2, neopixel.colors(NeoPixelColors.Blue))
         diody.show()
         basic.pause(50)
@@ -18,22 +19,23 @@ function jedna_dioda_kruh () {
         diody.show()
     }
 }
-input.onButtonPressed(Button.AB, function () {
+
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     rozsvit_duhu(false)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     tocici_duha()
 })
-function rozsvit_duhu (zhasni: boolean) {
+function rozsvit_duhu(zhasni: boolean) {
     diody.showRainbow(1, 360)
     if (zhasni) {
-        basic.pause(PRODLEVA)
+        basic.pause(100)
         diody.clear()
         diody.show()
     }
+    
 }
-let PRODLEVA = 0
-let diody: neopixel.Strip = null
+
+let diody : neopixel.Strip = null
 diody = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
-PRODLEVA = 100
 rozsvit_duhu(true)
