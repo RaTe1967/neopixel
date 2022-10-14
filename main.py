e4d475e5e1aac63fc8df965d3f9d690c4bb9550e
RaTe1967 <115639425+RaTe1967@users.noreply.github.com>
2022-10-14 09:26:42 +0200

    Aktualizovat pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 diody = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
 diody.show_rainbow(1, 360)
 
+def priklad_1():
+    for index in range(24):
+        diody.set_pixel_color(index, neopixel.colors(NeoPixelColors.BLUE))
+        diody.show()
+        basic.pause(50)
+        diody.set_pixel_color(index, neopixel.rgb(16, 16, 16))
+        diody.show()
+
 def on_forever():
     diody.show()
     diody.rotate(1)
--- main.ts
+++ main.ts
@@ -1,7 +1,25 @@
-let diody = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
-diody.showRainbow(1, 360)
-basic.forever(function on_forever() {
-    diody.show()
-    diody.rotate(1)
-    basic.pause(100)
+input.onButtonPressed(Button.A, function () {
+    tocici_kruh()
 })
+function tocici_kruh () {
+    for (let pořadí = 0; pořadí <= 80; pořadí++) {
+        diody.show()
+        diody.rotate(1)
+        basic.pause(100)
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    priklad_1()
+})
+function priklad_1 () {
+    for (let index = 0; index <= 7; index++) {
+        diody.setPixelColor(index, neopixel.colors(NeoPixelColors.Blue))
+        diody.show()
+        basic.pause(50)
+        diody.setPixelColor(index, neopixel.rgb(16, 16, 16))
+        diody.show()
+    }
+}
+let diody: neopixel.Strip = null
+diody = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
+diody.showRainbow(1, 360)
