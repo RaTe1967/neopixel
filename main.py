00a5e7cf03a4a005cc7b2beea6d56029e7545fb1
RaTe1967 <115639425+RaTe1967@users.noreply.github.com>
2022-10-14 09:02:30 +0200

    Aktualizovat pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-item = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
-item.show_rainbow(1, 360)
+diody = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
+diody.show_rainbow(1, 360)
 
 def on_forever():
-    item.show()
-    item.rotate(1)
+    diody.show()
+    diody.rotate(1)
     basic.pause(100)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let diody = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
 diody.showRainbow(1, 360)
-basic.forever(function () {
+basic.forever(function on_forever() {
     diody.show()
     diody.rotate(1)
     basic.pause(100)
