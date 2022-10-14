a6b0653187c495febda9dbe1357433982424f4bf
RaTe1967 <115639425+RaTe1967@users.noreply.github.com>
2022-10-14 11:24:52 +0200

    Aktualizovat pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     priklad_1()
 })
-function tocici_kruh () {
+function tocici_kruh() {
     diody.showRainbow(1, 360)
     for (let index = 0; index < 10; index++) {
         diody.show()
@@ -9,14 +9,17 @@ function tocici_kruh () {
         basic.pause(100)
     }
 }
-input.onButtonPressed(Button.AB, function () {
+
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    diody.clear()
+    basic.pause(500)
     diody.showRainbow(1, 360)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     tocici_kruh()
 })
-function priklad_1 () {
-    for (let index2 = 0; index2 <= 23; index2++) {
+function priklad_1() {
+    for (let index2 = 0; index2 < 24; index2++) {
         diody.setPixelColor(index2, neopixel.colors(NeoPixelColors.Blue))
         diody.show()
         basic.pause(50)
@@ -24,5 +27,6 @@ function priklad_1 () {
         diody.show()
     }
 }
-let diody: neopixel.Strip = null
+
+let diody : neopixel.Strip = null
 diody = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
