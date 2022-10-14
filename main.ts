3e8f3b4740ae91165188abe5a2c8a0c3de02ab5f
RaTe1967 <115639425+RaTe1967@users.noreply.github.com>
2022-10-14 12:24:36 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    priklad_1()
+function jezdici_dioda () {
+    for (let index2 = 0; index2 <= 7; index2++) {
+        diody.setPixelColor(index2, neopixel.colors(NeoPixelColors.Yellow))
+        diody.show()
+        basic.pause(100)
+        diody.setPixelColor(index2, neopixel.rgb(16, 16, 16))
+        diody.show()
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    jezdici_dioda()
 })
-function tocici_kruh() {
+function tocici_kruh () {
     diody.showRainbow(1, 360)
     for (let index = 0; index < 10; index++) {
         diody.show()
@@ -9,24 +18,11 @@ function tocici_kruh() {
         basic.pause(100)
     }
 }
-
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    diody.clear()
-    basic.pause(500)
+input.onButtonPressed(Button.AB, function () {
     diody.showRainbow(1, 360)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     tocici_kruh()
 })
-function priklad_1() {
-    for (let index2 = 0; index2 < 24; index2++) {
-        diody.setPixelColor(index2, neopixel.colors(NeoPixelColors.Blue))
-        diody.show()
-        basic.pause(50)
-        diody.setPixelColor(index2, neopixel.rgb(16, 16, 16))
-        diody.show()
-    }
-}
-
-let diody : neopixel.Strip = null
+let diody: neopixel.Strip = null
 diody = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
